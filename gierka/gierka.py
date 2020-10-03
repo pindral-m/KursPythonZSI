1cf13aaf6d782834c23b6ce8230a81e1dba6e793
Wojtycha <37120203+Wojtycha@users.noreply.github.com>
2020-10-03 12:45:41 +0200

    Add files via upload

--- gierka/gierka.py
+++ gierka/gierka.py
@@ -50,6 +50,41 @@ def secondStage(health):
 
     else:
         wrongChoiceMessage()
+
+def thirdStage(health):
+    print("Podchodzi do Ciebie dwu osobowy zespół złożony z wojownika i uzdrowiciela. Twój wybór: ")
+    playerChoice = int(input("1. Walcz\t2. Oddaj skarby\t3. Uciekaj: "))
+    if playerChoice == 1:
+        print("Kogo chcesz zaatakować?")
+        playerChoice = int(input("1. Wojownika\t2. Uzdrowiciela"))
+        if playerChoice == 1:
+            print("Podejmujesz walkę z wojownikiem, który jest leczony przez uzdrowiciela. Tracisz 50HP")
+            health -= 50
+            healthInfoMessage(health)
+
+        elif playerChoice == 2:
+            print("Uzdrowiciel ginie. Wojownik sam nie daje rady po walce z tobą ginie. Zjadasz obydwa trupy")
+            health += 25
+            healthInfoMessage(health)
+
+        else:
+            wrongChoiceMessage()
+
+    elif playerChoice == 2:
+        print("Udaje Ci się przekonać wroga do wzięcia łapówki")
+
+    elif playerChoice == 3:
+        print("Próbujesz uciekać")
+        generate = random.randint(1,2)
+        if generate == 1:
+            print("Udaje Ci się uciec!")
+
+        else:
+            print("Nrogowie Cię gonią! Co chcesz zrobić?")
+            playerChoice = int(input("1. Walcz\t2. Uciekaj: "))
+       
+    else:
+        wrongChoiceMessage()
 #---------STAGES---------#
 def healthInfoMessage(health):
     print("Twoje punkty zdrowia:", health)
@@ -63,6 +98,8 @@ def endGame():
 
 def main():
     welcome()
+    firstStage(health)
     secondStage(health)
+    thirdStage(health)
 
 main()
