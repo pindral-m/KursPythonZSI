3d499bb2f5cfc205c5868e1eea52412debfb9c62
Wojtycha <37120203+Wojtycha@users.noreply.github.com>
2020-10-03 12:26:33 +0200

    Add files via upload

--- gierka/gierka.py
+++ gierka/gierka.py
@@ -5,7 +5,7 @@ import random
 health = 100
 
 def welcome():
-    print("Witaj mimiku! Jesteś potworem o wyglądzie skrzyni, który zjada ludzi. W swoim życi musisz podjąć decyzje czy zjeść człowieka, ukryć się i oddać skarby albo uciec narażając się na pościg lub wykrycie.")
+    print("Witaj mimiku! Jesteś potworem o wyglądzie skrzyni, który zjada ludzi. W swoim życiu musisz podjąć decyzje czy zjeść człowieka, ukryć się i oddać skarby albo uciec narażając się na pościg lub wykrycie.")
     #playsound("")
 
 #---------STAGES---------#
@@ -25,14 +25,31 @@ def firstStage(health):
             health -= 10
             healthInfoMessage(health)
 
+    
+def secondStage(health):
+    print("Zbliża się do Ciebie Palladyn ubarany w błyszczącą zbroje,  wyposażony w magiczny, dwuręczny miecz, który dzierży w 1 ręce. Do tego widzisz tarcze w jaego drugiej ręce.  Jaką decyzje podejmiesz?")
+    playerChoice = int(input("1. Walcz\t2. Uciekaj\t3. Oddaj skarby: "))
+    if playerChoice == 1:
+        print("Podejmujesz walkę, ale z marnym skutkiem. Giniesz!")
+        endGame()
+
     elif playerChoice == 2:
-        print("Czekasz na następną ofiarę, z głodu tracisz 3 punkty zdrowia.")
-        health -= 3
+        print("Próbujesz uciekać")
+        generate = random.randint(1,2)
+        if generate == 1:
+            print("Palladyn cię dogania, tracisz 99HP")
+            health -= 99
+        else:
+            print("Udaje ci się uciec!")
+
+        healthInfoMessage(health)
+        
+    elif playerChoice == 3:
+        print("Udaje Ci się przekonać wroga do wzięcia łapówki")
         healthInfoMessage(health)
 
     else:
         wrongChoiceMessage()
-
 #---------STAGES---------#
 def healthInfoMessage(health):
     print("Twoje punkty zdrowia:", health)
@@ -41,8 +58,11 @@ def wrongChoiceMessage():
     dialogOptions = ["Nie kombinuj!", "Nie masz innych opcji!", "Ciekawość może zabić!"]
     print(random.choice(dialogOptions))
 
+def endGame():
+    pass
+
 def main():
     welcome()
-    firstStage(health)
+    secondStage(health)
 
 main()
